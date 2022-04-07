d7c23df40487f559adb03f0bfa84dd316e8949a4
Andrew Narvaez <anarvaez12@verizon.net>
2022-04-07 13:42:24 -0400

    validity checker

--- data/shows.js
+++ data/shows.js
@@ -2,18 +2,8 @@ const axios = require("axios");
 const validate = require("../validation.js");
 
 async function getActivitiesByName(name) {
-  // name = await validate.checkString(name, "Name Parameter");
-  // let url = "https://api.yelp.com/v3/businesses/search?term=" + name.toString +
-  //   "parks&latitude=40.745255&longitude=-74.034775";
-  //   const {data} = await axios.get(url);
-  //   slicedArray = data;
-  //   if(Object.keys(data).length > 5){
-  //     slicedArray = data.slice(0, 5);
-  //   }
-  //   slicedArray = Object.assign({}, slicedArray);
-  //   if(Object.keys(data).length < 1)
-  //     throw "There are no shows with that ID";
-  //   return slicedArray;
+  name = await validate.checkString(name, "Name Parameter");
+
   var data = "";
 
   var config = {
@@ -29,15 +19,6 @@ async function getActivitiesByName(name) {
     data: data,
   };
 
-  // axios(config)
-  //   .then(function (response) {
-  //     //console.log(JSON.stringify(response.data.businesses));
-
-  //     return (JSON.stringify(response.data.businesses));
-  //   })
-  //   .catch(function (error) {
-  //     console.log(error);
-  //   });
   let events = await axios(config);
   //console.log(JSON.stringify(events.data.businesses));
   return events.data.businesses;
