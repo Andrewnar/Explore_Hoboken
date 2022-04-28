af94da2bcde4afe123608753d3b309077bb7b520
Andrew Narvaez <anarvaez12@verizon.net>
2022-04-28 17:46:19 -0400

    Card Implmenetation
    We got cards!

--- app.js
+++ app.js
@@ -1,20 +1,28 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const static = express.static(__dirname + '/public');
+const static = express.static(__dirname + "/public");
 
-const configRoutes = require('./routes');
-const exphbs = require('express-handlebars');
+const configRoutes = require("./routes");
+const exphbs = require("express-handlebars");
 
-app.use('/public', static);
+app.use("/public", static);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }));
-app.set('view engine', 'handlebars');
+const Handlebars = require("handlebars");
+Handlebars.registerHelper("page", function (arr, pageSize, options) {
+  var result = [];
+  for (var i = 0; i < arr.length; i += pageSize) {
+    result.push(options.fn({ items: arr.slice(i, i + pageSize) }));
+  }
+  return result.join("");
+});
+app.engine("handlebars", exphbs.engine({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 configRoutes(app);
 
 app.listen(3000, () => {
   console.log("We've now got a server!");
-  console.log('Your routes will be running on http://localhost:3000');
+  console.log("Your routes will be running on http://localhost:3000");
 });
